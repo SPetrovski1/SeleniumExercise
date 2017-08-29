b5319536b7e9cdf7333d3bcc4179c7ec9f33afe9
SPetrovski1 <stefan.petrovski@endava.com>
2017-08-29 11:28:10 +0200

    ...

--- chromedriver/src/test/java/TestLogin.java
+++ chromedriver/src/test/java/TestLogin.java
@@ -46,16 +46,9 @@ public class TestLogin {
         button.click();
         Thread.sleep(2000);
         String url=driver.getCurrentUrl();
-        assertTrue(url.equals("http://www.phptravels.net/account/"));//done
-        int pom = 1;
-        /*Thread.sleep(2000);
-        WebElement logout=driver.findElement(By.id("logout"));
-        logout.click();
-        Thread.sleep(2000);
-        url=driver.getCurrentUrl();
-        assertTrue(url.equals("http://localhost:9000/#!/main"));
-        WebElement success=driver.findElement(By.id("logoutAlert"));
-        assertTrue(success.isDisplayed());*/
+        assertTrue(url.equals("http://www.phptravels.net/account/"));
+
+
     }
 
     @Test
@@ -88,73 +81,7 @@ public class TestLogin {
 
     }
 
-    /*@Test
-    public void testLoginNegativeWrongUsername() throws Exception {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-fullscreen");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:9000");
-        Thread.sleep(2000);
 
-        WebElement username=driver.findElement(By.id("username"));
-        username.sendKeys(user.getUsername().substring(0, ThreadLocalRandom.current().nextInt(user.getUsername().length()-3, user.getUsername().length()-1)));
-        Thread.sleep(2000);
-        WebElement password=driver.findElement(By.id("pwd"));
-        password.sendKeys(user.getPassword());
-        Thread.sleep(2000);
-        WebElement button=driver.findElement(By.className("btn"));
-        button.click();
-        Thread.sleep(2000);
-        WebElement alert=driver.findElement(By.id("warningAlert"));
-        assertTrue(alert.isDisplayed());
-    }
-    @Test
-    public void testLoginNegativeWrongPassword() throws Exception {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-fullscreen");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:9000");
-        Thread.sleep(2000);
-        User user=getRandom.getRandomUser();
-        WebElement username=driver.findElement(By.id("username"));
-        username.sendKeys(user.getUsername());
-        Thread.sleep(2000);
-        WebElement password=driver.findElement(By.id("pwd"));
-        password.sendKeys(user.getPassword().substring(0,ThreadLocalRandom.current().nextInt(user.getPassword().length()-2, user.getPassword().length()-1)));
-        Thread.sleep(2000);
-        WebElement button=driver.findElement(By.className("btn"));
-        button.click();
-        Thread.sleep(2000);
-        WebElement alert=driver.findElement(By.id("warningAlert"));
-        assertTrue(alert.isDisplayed());
-    }
-    @Test
-    public void testLoginNegativeEmpty() throws Exception {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-fullscreen");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:9000");
-        Thread.sleep(2000);
-        WebElement button=driver.findElement(By.className("btn"));
-        button.click();
-        Thread.sleep(2000);
-        WebElement alert=driver.findElement(By.id("warningAlert"));
-        assertTrue(alert.isDisplayed());
-    }
-
-    @Test
-    public void testRedirect() throws Exception {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-fullscreen");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:9000");
-        Thread.sleep(2000);
-        driver.get("http://localhost:9000/#!/main/home");
-        Thread.sleep(2000);
-        WebDriverWait wait=new WebDriverWait(driver,3);
-        wait.until(ExpectedConditions.alertIsPresent());
-        assertTrue(driver.switchTo().alert().getText().equals("No session valid! Redirecting to Login Page"));
-    }*/
 
     @After
     public void tearDown() throws Exception {
